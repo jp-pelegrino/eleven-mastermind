435eda6fdcaba1b7bdf3679cbbafae8ee2dfe5c9
Joemar Pastera Pelegrino <jppelegrino@up.edu.ph>
2025-11-12 02:03:05 +0800

    feat: enhance UI rendering, game flow

--- app.py
+++ app.py
@@ -2,39 +2,18 @@
 # To be run at start
 
 import user
-import spacing # for efficient border creation on the user interface
-import time
-import os
+import render
+import score
 
-# Print Startup Screen
-app_ui_01 = [""]
-app_ui_01.append("╔═══════════════════════════════════════════════════════╗")
-app_ui_01.append("║ 	┏━╸╻  ┏━╸╻ ╻┏━╸┏┓╻             			║")
-app_ui_01.append("║ 	┣╸ ┃  ┣╸ ┃┏┛┣╸ ┃┗┫ 🔴 🟢 🔵 🟡 ⚪ 🟠         	║")
-app_ui_01.append("║ 	┗━╸┗━╸┗━╸┗┛ ┗━╸╹ ╹       			║")
-app_ui_01.append("║   		 ┏┳┓┏━┓┏━┓╺┳╸┏━╸┏━┓┏┳┓╻┏┓╻╺┳┓  		║")
-app_ui_01.append("║ 	⚫⚪⚪⚫ ┃┃┃┣━┫┗━┓ ┃ ┣╸ ┣┳┛┃┃┃┃┃┗┫ ┃┃  	        ║")
-app_ui_01.append("║   		 ╹ ╹╹ ╹┗━┛ ╹ ┗━╸╹┗╸╹ ╹╹╹ ╹╺┻┛  		║")
-app_ui_01.append("╠═══════════════════════════════════════════════════════╣")
-app_ui_01.append("║     A Group Project of Group Eleven for CMSC 202 	║")
-app_ui_01.append("║        1st Semester A.Y. 2025-2026  			║")
-app_ui_01.append("╠═══════════════════════════════════════════════════════╣")
-app_ui_01.append("║	    [N] New Player    	 [L] Login  		║")
-app_ui_01.append("╚═══════════════════════════════════════════════════════╝")
-
-for i in range(len(app_ui_01)):
-    print(app_ui_01[i])
-    time.sleep(0.25)
-    continue
-
+render.menu_ui("",0.15,[""])
 
 while True:
     player_select = input("Select your option: ")
     if player_select.lower() == 'n':
-        player_id = user.reg("")
+        player = list(user.reg(""))
         break
     elif player_select.lower() == 'l':
-        player_id = user.login("")
+        player = list(user.login(""))
         break
     else:
         print("Invalid option selected.")
@@ -42,7 +21,10 @@ while True:
 # Open Game
 import game
 
-game.start_game(player_id)
+result = list(game.start_game(player[0],player[1],player[2]))
+
+# return (player_id,username,password,userscore)
+score.update_user_score(result[1],result[2],result[3])
 
 #Testing collaborators commit
 # Testing Pull Request
